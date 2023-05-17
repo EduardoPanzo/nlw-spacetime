3d49a1240b59485fe803428c9fbb385837dd0682
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-05-17 23:53:12 +0100

    feat:create table and endpoints

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,14 @@
 import fastify from 'fastify'
-import { prisma } from './lib/prisma'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories.routes'
 
 const app = fastify()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
+app.register(cors, {
+  origin: true,
 })
+
+app.register(memoriesRoutes)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('rodando em localhost:3333')
