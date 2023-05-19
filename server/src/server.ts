cb014116a4215109e18e732731526063ad2c1137
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-05-19 07:56:40 +0100

    feat: auth routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories.routes'
+import { authRoutes } from './routes/auth.routes'
+import jwt from '@fastify/jwt'
 
 const app = fastify()
 
@@ -8,6 +12,11 @@ app.register(cors, {
   origin: true,
 })
 
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app.listen({ port: 3333 }).then(() => {
