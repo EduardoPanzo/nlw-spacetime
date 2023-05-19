5d7d3ea0c608f47795fee10ba4681121e2518af0
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-05-19 08:54:48 +0100

    feat: register and  use and save token

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,13 +1,14 @@
+import { cookies} from 'next/headers'
 import { Copyright } from "@/components/Copyright";
 import { EmptyMemories } from "@/components/EmptyMemories";
 import { Hero } from "@/components/Hero";
 import { SignIn } from "@/components/SignIn";
-import { User } from "lucide-react";
+import { Profile } from '@/components/Profile';
 
-import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
+  const isAuthnticated = cookies().has('token')
+
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
@@ -18,7 +19,7 @@ export default function Home() {
         {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
-        <SignIn />
+        {!isAuthnticated?<SignIn />:<Profile/>}
 
         <Hero />
 
