8710d2758c3bfa1767f58cf70a739f6182e81be2
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-05-21 18:11:41 +0100

    feat: delete mobile project 1

--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -1,19 +1,27 @@
 import { getUser } from "@/lib/auth";
-import { User } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 export function Profile() {
-  const {name, avatarUrl} =  getUser()
+  const { name, avatarUrl } = getUser();
   return (
-    <div
-      className="flex items-center gap-3 text-left "
-    >
-        <Image src={avatarUrl} width={40} height={40} alt="" className="h-10 w-10 rounded-full" />
+    <div className="flex items-center gap-3 text-left ">
+      <Image
+        src={avatarUrl}
+        width={40}
+        height={40}
+        alt=""
+        className="h-10 w-10 rounded-full"
+      />
 
       <p className="max-w-[140px] text-sm leading-snug">
         {name}
-        <Link href={''} className="block text-red-400 hover:text-red-300">Quero sair</Link>
+        <a
+          href={"/api/auth/logout/"}
+          className="block text-red-400 hover:text-red-300"
+        >
+          Quero sair
+        </a>
       </p>
     </div>
   );
